1f80fce833ffc99e5d273ce4c18cb0ae21d85a8d
Diones <pereiradiones987@gmail.com>
2025-08-11 16:39:21 -0300

    atualizando validação de body

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug', 'verbose'],
   });
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, //Converte tipos automaticamente
+    transform: true, //Remove campos que não estão no DTO
+    forbidNonWhitelisted: true // Erro se vier campo desconhecido
+  }));
   await app.listen(process.env.PORT ?? 3000);
   Logger.log(`Servidor rodando em http://localhost:${process.env.PORT ?? 3000}`);
 }
