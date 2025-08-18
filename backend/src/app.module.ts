f0e00c6feb966b345d52f16b35bb3774e5a86290
Diones <pereiradiones987@gmail.com>
2025-08-18 16:15:51 -0300

    configurando variaveis de ambiente

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,19 @@ import { BannersModule } from './banners/banners.module';
 import { ProductsModule } from './products/products.module';
 import { CategoryModule } from './category/category.module';
 import { CartModule } from './cart/cart.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, BannersModule, ProductsModule, CategoryModule, CartModule],
+  imports: [
+    PrismaModule,
+    BannersModule,
+    ProductsModule,
+    CategoryModule,
+    CartModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    })
+  ],
   controllers: [],
   providers: [],
 })
