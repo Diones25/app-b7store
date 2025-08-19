ea17f207d53f894ca1e1566b348175c9638106a6
Diones <pereiradiones987@gmail.com>
2025-08-19 16:26:15 -0300

    auth controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthRegisterDTO } from './dto/auth-register.dto';
+import { AuthService } from './auth.service';
+import { AuthLoginDTO } from './dto/auth-login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('login')
+  login(@Body() body: AuthLoginDTO) {
+    return this.authService.login(body.email, body.password);
+  }
+  
+  @Post('register')
+  register(@Body() body: AuthRegisterDTO) {
+    return this.authService.register(body);
+  }
+}
---
+++ backend/src/auth/dto/auth-register.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsString, MinLength } from "class-validator";
+
+export class AuthRegisterDTO {
+
+  @IsString({ message: 'Nome deve ser uma string' })
+  @MinLength(2, { message: 'Nome deve ter no minimo 2 caracteres' })
+  name: string;
+
+  @IsString({ message: 'Email deve ser uma string' })
+  @IsEmail()
+  email: string;
+
+  @IsString({ message: 'Senha deve ser uma string' })
+  @MinLength(4, { message: 'Password deve ter no minimo 4 caracteres' })
+  password: string;
+}
