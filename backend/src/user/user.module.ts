f6a407d541205916be7ae740c1eb874e36d2a8e3
Diones <pereiradiones987@gmail.com>
2025-08-19 16:24:30 -0300

    removendo rota não utilizada

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserController } from './user.controller';
 
 @Module({
   providers: [UserService],
-  controllers: [UserController],
+  controllers: [],
   exports: [UserService]
 })
 export class UserModule {}
