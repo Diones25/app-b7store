cb8b473b85b786ac0bebf69f6ecdb9145a3172c4
Diones <pereiradiones987@gmail.com>
2025-08-19 15:52:28 -0300

    importando modulo de usuarios

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -2,11 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [JwtModule.register({
-    secret: process.env.JWT_SECRET || 'secret',
-  })],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secret',
+    }),
+    UserModule
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
