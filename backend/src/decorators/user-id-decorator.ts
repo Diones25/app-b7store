c2bfdfde0e7cc1800c2d9a0a5d4c1e7eabcdb054
Diones <pereiradiones987@gmail.com>
2025-08-20 12:07:03 -0300

    decotaror para pegar o id de usuário do token jwt

---
+++ backend/src/decorators/user-id-decorator.ts
@@ -0,0 +1,14 @@
+/* istanbul ignore file */
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { authorizationToLoginPayload } from "src/utils/base-64-converter";
+
+
+export const UserId = createParamDecorator(
+  (_, ctx: ExecutionContext) => {
+    const { authorization } = ctx.switchToHttp().getRequest().headers;
+
+    const loginPayload = authorizationToLoginPayload(authorization);
+
+    return loginPayload?.id;
+  }
+)
---
+++ backend/src/utils/base-64-converter.ts
@@ -0,0 +1,10 @@
+
+export const authorizationToLoginPayload = (authorization: string) => {
+  const authorizationSplited = authorization.split('.');
+
+  if (authorizationSplited.length < 3 || !authorizationSplited[1]) {
+    return undefined;
+  }
+
+  return JSON.parse(Buffer.from(authorizationSplited[1], 'base64').toString('ascii'));
+}
