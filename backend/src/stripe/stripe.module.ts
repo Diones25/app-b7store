580fd5f99a5aee27563e1ab41b68df9fd58bad10
Diones <pereiradiones987@gmail.com>
2025-09-02 17:21:57 -0300

    corrigindo erro de imports de module

--- backend/src/stripe/stripe.module.ts
+++ backend/src/stripe/stripe.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { StripeService } from './stripe.service';
+import { ProductsModule } from 'src/products/products.module';
 
 @Module({
+  imports: [ProductsModule],
   providers: [StripeService],
   exports: [StripeService]
 })
