9c6ec9514fd01953d010a21cf989d18377a9018d
Diones <pereiradiones987@gmail.com>
2025-09-03 23:17:53 -0300

    Ajustando função authorizationToLoginPayload para evitar erro 500

--- backend/src/utils/base-64-converter.ts
+++ backend/src/utils/base-64-converter.ts
@@ -1,10 +1,19 @@
+import { UnauthorizedException } from "@nestjs/common";
 
 export const authorizationToLoginPayload = (authorization: string) => {
+  if (!authorization) {
+    throw new UnauthorizedException('Cabeçalho Authorization não encontrado');
+  }
+
   const authorizationSplited = authorization.split('.');
 
   if (authorizationSplited.length < 3 || !authorizationSplited[1]) {
-    return undefined;
+    throw new UnauthorizedException('Token inválido');
   }
 
-  return JSON.parse(Buffer.from(authorizationSplited[1], 'base64').toString('ascii'));
+  try {
+    return JSON.parse(Buffer.from(authorizationSplited[1], 'base64').toString('ascii'));
+  } catch (e) {
+    throw new UnauthorizedException('Erro ao decodificar o token');
+  }
 }
