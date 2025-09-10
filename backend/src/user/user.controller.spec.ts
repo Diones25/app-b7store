798725fced20a8834c9d8f358889bb5cc9fe461d
Diones <pereiradiones987@gmail.com>
2025-09-10 13:48:48 -0300

    Testes contoller de usuários

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { UserController } from "./user.controller";
 import { UserService } from "./user.service";
 import { AuthService } from "../auth/auth.service";
+import { CreateAdresseUserDto } from "./dto/create-adress-user.dto";
 
 describe('UserController', () => {
   let userController: UserController;
@@ -51,6 +52,36 @@ describe('UserController', () => {
 
   it('should be defined', () => {
     expect(userController).toBeDefined();
+  });
+
+  it('Criar endereço', async () => {
+    const body: CreateAdresseUserDto = {
+      zipcode: '62320005',
+      street: 'Rua de teste',
+      number: '123',
+      city: 'Tianguá',
+      state: 'Ceará',
+      country: 'Brasil',
+      complement: 'Complemento de teste'
+    }
+    const userId = 1;
+    await userController.createAdresses(userId, body);
+    expect(mockUserService.createAdress).toHaveBeenCalledTimes(1);
+    expect(mockUserService.createAdress).toHaveBeenCalledWith(userId, body);
+  });
+
+  it('Buscando um usuário', async () => {
+    const id = 1;
+    await userController.findOne(id);
+    expect(mockUserService.findOne).toHaveBeenCalledTimes(1);
+    expect(mockUserService.findOne).toHaveBeenCalledWith(id);
+  });
+
+  it('Buscando um endereço', async () => {
+    const userId = 1;
+    await userController.findAdresses(userId);
+    expect(mockUserService.findAdresses).toHaveBeenCalledTimes(1);
+    expect(mockUserService.findAdresses).toHaveBeenCalledWith(userId);
   });
 
 });
