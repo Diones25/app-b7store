b123ac8098c478e2bbe39053be1ccb96ad8b73cc
Diones <pereiradiones987@gmail.com>
2025-09-11 21:22:05 -0300

    testes auth.controller

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { AuthLoginDTO } from './dto/auth-login.dto';
+import { AuthRegisterDTO } from './dto/auth-register.dto';
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authcontroller: AuthController;
+
+  const mockAuthService = {
+    login: jest.fn(),
+    register: jest.fn()
+  }
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: mockAuthService
+        }
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authcontroller = module.get<AuthController>(AuthController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authcontroller).toBeDefined();
+  });
+
+  it('Login', async () => {
+    const body: AuthLoginDTO = {
+      email: '62320005',
+      password: '1234'
+    }
+    await authcontroller.login(body);
+    expect(mockAuthService.login).toHaveBeenCalledTimes(1);
+    expect(mockAuthService.login).toHaveBeenCalledWith(body.email, body.password);
+  });
+
+  it('Register', async () => {
+    const body: AuthRegisterDTO = {
+      name: 'Teste',
+      email: '62320005',
+      password: '1234'
+    }
+    await authcontroller.register(body);
+    expect(mockAuthService.register).toHaveBeenCalledTimes(1);
+    expect(mockAuthService.register).toHaveBeenCalledWith(body);
   });
 });
