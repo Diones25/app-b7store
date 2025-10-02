9928071ed621074c68f66a6278e9c037c376a42f
Diones <pereiradiones987@gmail.com>
2025-10-02 11:00:27 -0300

    swagger BannersController

--- backend/src/banners/banners.controller.ts
+++ backend/src/banners/banners.controller.ts
@@ -1,12 +1,31 @@
 import { Controller, Get } from '@nestjs/common';
 import { BannersService } from './banners.service';
 import { getAbsoluteImageUrl } from '../utils/get-absolute-image-url';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('banners')
 export class BannersController {
   constructor(private readonly bannersService: BannersService) { }
   
   @Get()
+  @ApiOperation({
+    summary: 'Listar banners',
+    description: 'Retorna uma lista de banners cadastrados no sistema',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Banners encontrados com sucesso',
+    example: [
+      {
+        "img": "http://localhost:3001/media/banners/banner_promo_1.jpg",
+        "link": "/categories/camisas"
+      },
+      {
+        "img": "http://localhost:3001/media/banners/banner_promo_2.jpg",
+        "link": "/categories/algo"
+      }
+    ]
+  })
   async findAll() {
     const banners = this.bannersService.findAll();
 
