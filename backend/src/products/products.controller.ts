ee1dc95363d29f9d2bda956f6db74bce0a187b45
Diones <pereiradiones987@gmail.com>
2025-10-02 11:49:20 -0300

    swagger ProductsController

--- backend/src/products/products.controller.ts
+++ backend/src/products/products.controller.ts
@@ -5,7 +5,7 @@ import { getAbsoluteImageUrl } from '../utils/get-absolute-image-url';
 import { ProductIdParamDto } from './dto/productId-param-dto';
 import { ProductIdParamRelatedDto } from './dto/productId-param-related-dto';
 import { ProductsQueryRelatedDto } from './dto/products-query-related-dto';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 
 @Controller('products')
 export class ProductsController {
@@ -59,6 +59,49 @@ export class ProductsController {
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Listar produto',
+    description: 'Retorna um objeto de produtos cadastrados no sistema',
+  })
+  @ApiParam({ name: 'id', type: Number, required: true, description: 'ID do produto' })
+  @ApiResponse({
+    status: 200,
+    description: 'Produto encontrado com sucesso',
+    example: {
+      "id": 1,
+      "label": "Camisa PHP",
+      "price": 69.9,
+      "description": "Camisa com estampa PHP, para desenvolvedores web",
+      "categoryId": 1,
+      "images": [
+        "http://localhost:3001/media/products/product_1_2.jpg",
+        "http://localhost:3001/media/products/product_1_1.jpg"
+      ],
+      "category": {
+        "id": 1,
+        "name": "Camisas",
+        "slug": "camisas"
+      }
+    }
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro na validação dos dados',
+    example: {
+      message: 'id deve conter apenas números',
+      error: 'Bad Request',
+      statusCode: 400
+    }
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Produto não encontrado',
+    example: {
+      message: 'Produto com id 6 não encontrado.',
+      error: 'Not Found',
+      statusCode: 404
+    }
+  })
   async findOne(@Param() params: ProductIdParamDto) {
     const product = await this.productsService.findOne(parseInt(params.id));
     if (!product) return null;
@@ -69,6 +112,49 @@ export class ProductsController {
   }
 
   @Get(':id/related')
+  @ApiOperation({
+    summary: 'Listar produtos relacionados',
+    description: 'Retorna uma lista de produtos relacionados cadastrados no sistema apartir de um id da categoria',
+  })
+  @ApiParam({ name: 'id', type: Number, required: true, description: 'ID da categoria' })
+  @ApiResponse({
+    status: 200,
+    description: 'Produtos encontrados com sucesso',
+    example: [
+      {
+        "id": 3,
+        "label": "Camisa React",
+        "price": 94.5,
+        "image": "http://localhost:3001/media/products/product_3_1.jpg",
+        "liked": false
+      },
+      {
+        "id": 2,
+        "label": "Camisa Python",
+        "price": 79.99,
+        "image": "http://localhost:3001/media/products/product_2_2.jpg",
+        "liked": false
+      }
+    ]
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Erro na validação dos dados',
+    example: {
+      message: 'id deve conter apenas números',
+      error: 'Bad Request',
+      statusCode: 400
+    }
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Produto não encontrado',
+    example: {
+      message: 'Produto com id 6 não encontrado.',
+      error: 'Not Found',
+      statusCode: 404
+    }
+  })
   async findRelated(@Param() params: ProductIdParamRelatedDto, @Query() query: ProductsQueryRelatedDto) {
     const products = await this.productsService.findRelated(parseInt(params.id), query.limit ? parseInt(query.limit) : undefined);
 
