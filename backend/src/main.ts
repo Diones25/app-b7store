54ec902a499f6de6a89a3a6f36be8f718fb47d5f
Diones <pereiradiones987@gmail.com>
2025-10-02 10:52:01 -0300

    configuração inicial do swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as express from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -15,6 +16,26 @@ async function bootstrap() {
     transform: true, //Remove campos que não estão no DTO
     forbidNonWhitelisted: true // Erro se vier campo desconhecido
   }));
+
+  const config = new DocumentBuilder()
+    .setTitle('B7Store - API')
+    .setDescription('O B7Store é uma api de ecomerce, com gerenciamento de banners, produtos, carrinho e integração com o Stripe para gerenciamento de pagamentos.')
+    .setVersion('1.0')
+    .addBearerAuth( 
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Insira o token JWT para autenticar',
+        in: 'header',
+      },
+      'JWT-auth', 
+    )
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory()); 
+
   await app.listen(process.env.PORT ?? 3000);
   Logger.log(`Servidor rodando em http://localhost:${process.env.PORT ?? 3000}`);
 }
